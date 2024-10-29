0eb3e5e550855f879e37b046902b6c00dc800611
V B Sakthivel <112497833+V-B-SAKTHIVEL@users.noreply.github.com>
2024-10-29 15:19:37 +0530

    Add files via upload

---
+++ jsDataTypes.js
@@ -0,0 +1,19 @@
+//Learning the datatypes in JS
+
+let myNum = 20;
+console.log(myNum);
+console.log(typeof myNum);
+
+let myString = "JavaScript";
+console.log(myString);
+console.log(typeof myString);
+
+let jsIsFun = true;
+console.log(jsIsFun);
+console.log(typeof jsIsFun);
+
+let empVar;
+console.log(empVar);
+console.log(typeof empVar);
+
+console.log(null);
