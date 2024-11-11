8a4b809b45073602c1e8f3a4a917fe02f5ef8b16
V B Sakthivel <112497833+V-B-SAKTHIVEL@users.noreply.github.com>
2024-11-11 21:47:03 +0530

    Create BMICalculator.js

---
+++ BMICalculator.js
@@ -0,0 +1,14 @@
+//Just an example of calculating BMI for the predefined values. 
+const massMark = 78;
+const heightMark = 1.69;
+
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+
+console.log(BMIMark, BMIJohn);
+
+const markHigherBMI = BMIMark > BMIJohn;
+console.log(markHigherBMI);
