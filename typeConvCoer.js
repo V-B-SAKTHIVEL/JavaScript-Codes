90c4235c0c017ccd50a93f574005f1d016126c78
V B Sakthivel <112497833+V-B-SAKTHIVEL@users.noreply.github.com>
2024-11-12 14:23:10 +0530

    Create typeConvCoer.js

---
+++ typeConvCoer.js
@@ -0,0 +1,12 @@
+//Type Conversion
+const inputYear = '1991';
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 20);
+
+console.log(Number('Sakthi'));
+console.log(typeof NaN);
+
+//Type Coercion
+console.log('I am ' + 23 + ' years old');
+console.log('20' - '10' - 5);
+console.log('20' * '2');
