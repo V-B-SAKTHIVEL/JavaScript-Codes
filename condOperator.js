f52214251a6c86dfc733cd5fdb4a44b981959253
V B Sakthivel <112497833+V-B-SAKTHIVEL@users.noreply.github.com>
2024-11-15 20:32:48 +0530

    Create condOperator.js

---
+++ condOperator.js
@@ -0,0 +1,4 @@
+const age = 10;
+age >= 18 ? console.log('I am eligible for voting') : console.log('I am not eligible for voting');
+
+console.log(`I am ${age} years old, so I can ${age >= 18 ? 'vote' : 'not vote'}`);
